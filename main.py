5c416f23dc9ae3ec470d2657204e54632d6052c1
Zack Dowd <zdowd2796@gmail.com>
2023-11-30 16:58:17 -0700

    balls bouncing off paddles properly

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ while game_is_on:
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         #needs to bounce
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(paddle_r) < 50 and ball.xcor() > 320 or ball.distance(paddle_l) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect collision with l_paddle
 
 screen.exitonclick()
