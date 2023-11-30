c6582570e8451488b2859412a329267cb15f0d1e
Zack Dowd <zdowd2796@gmail.com>
2023-11-30 15:15:52 -0700

    second paddle added and working

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 from turtle import Screen, Turtle
-
+from paddle import Paddle
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle(shape="square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def move_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def move_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+paddle_l = Paddle((-350, 0))
+paddle_r = Paddle((350, 0))
 
 screen.listen()
-screen.onkey(move_up, "Up")
-screen.onkey(move_down, "Down")
+screen.onkeypress(paddle_l.move_up, "w")
+screen.onkeypress(paddle_l.move_down, "s")
+screen.onkeypress(paddle_r.move_up, "Up")
+screen.onkeypress(paddle_r.move_down, "Down")
 
 game_is_on = True
 while game_is_on:
